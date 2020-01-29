526e75b8e237c83ed3f1e337c8870184b2ae4510
Tygr <workfile975@gmail.com>
2020-01-29 20:57:14 +0800

    add two create process.

--- WindowsPractice/CreateProcessAPI/Source.cpp
+++ WindowsPractice/CreateProcessAPI/Source.cpp
@@ -1,15 +1,27 @@
 #include <Windows.h>
+#include <iostream>
 
-bool WinExec_Test(char , UINT);
-bool ShellExecute_Test(LPCWSTR, UINT);
-bool CreateProcess_Test(LPWSTR, UINT uiCmdShow);
+bool WinExec_Test(LPCSTR pszExePath, UINT uiCmdShow);
+bool ShellExecute_Test(LPCWSTR pszExePath, UINT uiCmdShow);
+BOOL CreateProcess_Test(LPWSTR pszExePath, UINT uiCmdShow);
 
 int main(){
+	
+	if (WinExec_Test("HelloWorld.exe", SW_SHOWNORMAL))
+		std::cout << "Win exec successful." << std::endl;
+		
+	if (ShellExecute_Test(L"HelloWorld.exe", SW_SHOWNORMAL))
+		std::cout << "Shell execute successful." << std::endl;
+	
+	//不知明原因無法成功,待解決.
+	//if (CreateProcess_Test(L"HelloWorld.exe", SW_SHOWNORMAL))
+	//	std::cout << "Create process successful." << std::endl;
 
+	system("pause");
 	return 0;
 }
 
-bool WinExec_Test(char *pszExePath, UINT uiCmdShow){
+bool WinExec_Test(LPCSTR pszExePath, UINT uiCmdShow){
 	UINT uiRet = NULL;
 	uiRet = WinExec(pszExePath, uiCmdShow);
 	if (31 > uiRet)
@@ -27,18 +39,36 @@ bool ShellExecute_Test(LPCWSTR pszExePath, UINT uiCmdShow){
 		return FALSE;
 }
 
-bool CreateProcess_Test(LPWSTR pszExePath, UINT uiCmdShow){
+BOOL CreateProcess_Test(LPWSTR pszExePath, UINT uiCmdShow){
 	STARTUPINFO si = { 0 };
 	PROCESS_INFORMATION pi;
-	bool bRet = FALSE;
-	si.cb = sizeof(si);
-	si.dwFlags = STARTF_USESHOWWINDOW;	//指定 wShowWindow 成員有效
-	si.wShowWindow = uiCmdShow;
-	bRet = CreateProcess(NULL, pszExePath, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi);
+	BOOL bRet = FALSE;
 
-	if (bRet){
-		CloseHandle(pi.hThread);
-		CloseHandle(pi.hProcess);
+	ZeroMemory(&si, sizeof(si));
+	ZeroMemory(&pi, sizeof(pi));
+
+	si.cb = sizeof(si);
+	si.dwFlags = STARTF_USESHOWWINDOW; //指定 wShowWindow 成員有效
+	si.wShowWindow = uiCmdShow;
+
+	bRet = CreateProcess(NULL,   // No module name (use command line)
+		pszExePath,        // Command line
+		NULL,           // Process handle not inheritable
+		NULL,           // Thread handle not inheritable
+		FALSE,          // Set handle inheritance to FALSE
+		0,              // No creation flags
+		NULL,           // Use parent's environment block
+		NULL,           // Use parent's starting directory 
+		&si,            // Pointer to STARTUPINFO structure
+		&pi);           // Pointer to PROCESS_INFORMATION structure
+	WaitForSingleObject(pi.hProcess, INFINITE);
+	if (bRet)
+	{
+		::CloseHandle(pi.hThread);
+		::CloseHandle(pi.hProcess);
+		return TRUE;
 	}
-	return bRet;
+	
+
+	return FALSE;
 }
